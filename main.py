d07c9b8ef60a1f3f4e40acae2629d02cf96d303c
Rohit Kumar <gahlyanrohit18@gmail.com>
2023-04-17 02:03:21 +0530

    setup app & make routes

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/reverse")
+async def string_reverse(input:str):
+    return input[::-1]
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
